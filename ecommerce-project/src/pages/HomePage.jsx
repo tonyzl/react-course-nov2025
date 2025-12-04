9627436b56680375568fbf86c51b70a1232c7fff
tonyzl <germanantoniosiete@gmail.com>
2025-12-04 09:00:29 -0500

    Split header into a component and  use Link

--- ecommerce-project/src/pages/HomePage.jsx
+++ ecommerce-project/src/pages/HomePage.jsx
@@ -1,42 +1,16 @@
+
+import { Header } from '../components/Header';
 import './Header.css'
 import './HomePage.css'
+import '../components/Header'
 
 export function HomePage() {
     return (
         <>
             <title>Ecommerce Project</title>
 
-            <div className="header">
-                <div className="left-section">
-                    <a href="/" className="header-link">
-                        <img className="logo"
-                            src="images/logo-white.png" />
-                        <img className="mobile-logo"
-                            src="images/mobile-logo-white.png" />
-                    </a>
-                </div>
+            <Header/>
 
-                <div className="middle-section">
-                    <input className="search-bar" type="text" placeholder="Search" />
-
-                    <button className="search-button">
-                        <img className="search-icon" src="images/icons/search-icon.png" />
-                    </button>
-                </div>
-
-                <div className="right-section">
-                    <a className="orders-link header-link" href="/orders">
-
-                        <span className="orders-text">Orders</span>
-                    </a>
-
-                    <a className="cart-link header-link" href="/checkout">
-                        <img className="cart-icon" src="images/icons/cart-icon.png" />
-                        <div className="cart-quantity">3</div>
-                        <div className="cart-text">Cart</div>
-                    </a>
-                </div>
-            </div>
 
             <div className="home-page">
                 <div className="products-grid">
